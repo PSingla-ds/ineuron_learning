6b785057223d9341bd6099bd2434b8fb60186328
PS <psinglads@gmail.com>
2021-09-20 13:45:24 -0700

    all running files

--- and.py
+++ and.py
@@ -1,29 +1,24 @@
-from utils.model import Perceptron 
-from utils.all_utils import prepare_data, save_model, save_plot
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
-import numpy as np
 
 
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(data)
+    print(df)
+    X, y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+    _ = model.total_loss()
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
 
-AND = { 
-    'x1': [0,0,1,1],
-    'x2': [0,1,0,1],
-    'y': [0,0,0,1]
-       }
-
-df_and = pd.DataFrame(AND)
-print(df_and)
-
-
-X, y = prepare_data(df_and)
-
-ETA = 0.3
-EPOCHS = 10
-
-model_and = Perceptron(eta = ETA, epochs = EPOCHS)
-model_and.fit(X, y)
-
-_ = model_and.total_loss()
-
-save_model(model_and, filename = "and.model")
-save_plot(df_and, "and.png", model_and)
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
